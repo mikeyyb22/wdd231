3b8583f585ec2547d708b339ab6c06a5918145a4
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-04 16:32:07 -0300

    Added footer

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,13 @@
 const navButton = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
+  navBar.classList.toggle('show');
 });
+
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = today.getFullYear();
+document.getElementById("lastModified").textContent = `Last modification: ${document.lastModified}`;
+
