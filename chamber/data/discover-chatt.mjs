c6c253dd312fa8c7a44f95352a4e06d27f4b81fe
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-29 01:40:45 -0300

    added cards for attractions

---
+++ chamber/data/discover-chatt.mjs
@@ -0,0 +1,52 @@
+const destinations = [
+    {
+        "name": "Tennessee Aquarium",
+        "address": "1 Broad St, Chattanooga, TN 37402",
+        "description": "A top-rated facility featuring two buildings, River Journey and Ocean Journey. It houses over 12,000 animals, including freshwater species, sharks, penguins, and lemurs, showcasing a \"mountains-to-the-sea\" journey of water.",
+        "photourl": "tennessee-aquarium.webp"
+    },
+    {
+        "name": "Rock City",
+        "address": "1400 Patten Rd, Lookout Mountain, GA 30750",
+        "description": "A renowned 1930s-era natural attraction featuring massive ancient rock formations, botanical gardens, and scenic trails. Home of the \"Seven States View,\" where seven states can be seen from one vantage point.",
+        "photourl": "rock-city.webp"
+    },
+    {
+        "name": "Incline Railway",
+        "address": "3917 St Elmo Ave, Chattanooga, TN 37409",
+        "description": "Known as \"America's Most Amazing Mile,\" it is a historic, 130-year-old funicular that climbs Lookout Mountain at a breathtaking 72.7% grade, making it one of the steepest passenger railways in the world.",
+        "photourl": "incline-railway.webp"
+    },
+    {
+        "name": "Ruby Falls",
+        "address": "1720 S. Scenic Highway, Chattanooga, TN 37409",
+        "description": "A 63 feet tall waterfall in Ruby Falls Cave of Lookout Mountain. The cave is commercialized with electric lights, paved foot trails, and an elevator for access.",
+        "photourl": "ruby-falls.webp"
+    },
+    {
+        "name": "Tennessee Valley Railroad Museum",
+        "address": "4119 Cromwell Rd, Chattanooga, TN 37421",
+        "description": "Offers visitors a unique experience with Chattanooga and Delano train rides on vintage train cars.",
+        "photourl": "tv-rail-museum.webp"
+    },
+    {
+        "name": "Walnut Street Bridge",
+        "address": "1 Walnut St, Chattanooga, TN 37403",
+        "description": "Pedestrian bridge built in 1890, the 2,376-foot-long bridge connects Chattanooga's downtown to North Chattanooga. Known as the first non-military highway bridge across the Tennessee River.",
+        "photourl": "walnut-bridge.webp"
+    },
+    {
+        "name": "The Coker Museum",
+        "address": "1309 Chestnut St, Chattanooga, TN 37402",
+        "description": "An automotive history museum that showcases over 100 vintage cars, motorcycles, buses, and airplanes, including Corky Coker's personal collection",
+        "photourl": "coker-museum.webp"
+    },
+    {
+        "name": "Hunter Museum of American Art",
+        "address": "10 Bluff View Ave, Chattanooga, TN 37403",
+        "description": "Museum works  include works representing the Hudson River School, 19th century genre painting, American Impressionism, the Ashcan School, early modernism, regionalism, and post-World War II modern and contemporary art.",
+        "photourl": "hunter-museum.webp"
+    }
+]
+
+export { destinations };
---
+++ chamber/scripts/discover.js
@@ -0,0 +1,45 @@
+import { destinations } from '../data/discover-chatt.mjs'
+
+// console.log(destinations);
+
+const allPlaces = document.querySelector("#allplaces");
+
+function showPlaces(places) {
+    places.forEach((place) => {
+        let name = place.name;
+        let address = place.address;
+        let description = place.description;
+        let url = place.photourl;
+        let fullUrl = `./images/${url}`;
+
+        const div = document.createElement("div");
+        const h2 = document.createElement("h2");
+        const fig = document.createElement("figure");
+        const add = document.createElement("address");
+        const p = document.createElement("p");
+        const button = document.createElement("button");
+        const img = document.createElement("img");
+        img.src = fullUrl;
+        img.alt = `${description}`;
+        button.type = "button";
+        button.textContent = "Learn More";
+
+
+        h2.textContent = name;
+        add.textContent = address;
+        p.textContent = description;
+
+        fig.appendChild(img);
+
+        div.appendChild(h2);
+        div.appendChild(fig);
+        div.appendChild(add);
+        div.appendChild(p);
+        div.appendChild(button);
+
+        allPlaces.appendChild(div);
+
+    });
+}
+
+showPlaces(destinations);
